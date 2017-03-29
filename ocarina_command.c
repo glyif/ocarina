5270d7b630f972ea156a78c44c7b110c2fcae76b
Bobby Yang <122@holbertonschool.com>
2017-03-29 07:41:49 +0000

    updated with prev command

--- ocarina_command.c
+++ ocarina_command.c
@@ -6,12 +6,26 @@ int ocarina_command(link *ocarina, link *history)
 	link *tmp;
 	link *recall_history;
 	int integer;
+	int counter;
+	int i;
 
+	i = 0;
+	stop = list_len(history);
+	counter = list_len(history);
 	tmp = get_nodeint_at_index(ocarina, 1);
 
 	if (tmp == NULL)
 	{
 		printf("not enought arguments\n");
+	}
+	else if (_strcmp(tmp->command, "prev") == 0)
+	{
+		counter--;
+		recall_history = get_nodeint_at_index(history, counter);
+		if (counter < 0)
+			printf("that's all folks");
+		else
+			printf("%s\n", recall_history->command);
 	}
 	else
 	{
